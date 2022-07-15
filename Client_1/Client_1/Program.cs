33f871f8e03ed4f4ef37858eb931a856b1b6538a
lukas <willmann.Lukas@web.de>
2022-07-15 17:29:37 +0200

    TCP Server and Client

---
+++ Client_1/Client_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Net.Sockets;
+
+var client = new TcpClient("127.0.0.1", 9999);
+
+NetworkStream networkStream = client.GetStream();
+
+var writer = new StreamWriter(networkStream);
+using var reader = new StreamReader(networkStream, Encoding.UTF8);
+
+var message = "hello server";
+Console.WriteLine(message);
+byte[] bytes = Encoding.UTF8.GetBytes(message);
+networkStream.Write(bytes, 0, bytes.Length);
+
+StreamReader sr = new StreamReader(networkStream);
+string response = sr.ReadLine();
+Console.WriteLine(response);
+
+networkStream.Close();
+client.Close();
+Console.ReadKey();
---
+++ TCPServer_1/TCPServer_1/TCPServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;      //required
+using System.Net.Sockets;    //required
+
+namespace ServerTest
+{
+    class TCPServer
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+            // we set our IP address as server's address, and we also set the port: 9999
+
+            server.Start();  // this will start the server
+
+            while (true)   //we wait for a connection
+            {
+                Console.WriteLine("aha");
+                TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+
+                NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+                //StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+
+
+                if (client.Connected)  //while the client is connected, we look for incoming messages
+                {
+                    byte[] msg = new byte[1024];     //the messages arrive as byte array
+                    ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+
+                    Console.WriteLine(Encoding.Default.GetString(msg)); //now , we write the message as string
+
+                    byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
+                    hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
+
+                    sw.WriteLine("hello");     //sending the message
+                    sw.Flush();
+                }
+            }
+        }
+    }
+
+    
+}
+
